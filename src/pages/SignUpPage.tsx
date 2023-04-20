92b9127f6c53a6f966f47652912572e745049ef3
lsenaa <lsenaa3838@gmail.com>
2023-04-20 16:44:55 +0900

    feat: 회원가입 폼 추가

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,4 +1,52 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { postRegister } from "../apis/users/users";
+
+interface IFormRegisterData {
+  email: string;
+  username: string;
+  password: string;
+}
+
 const SignUpPage = () => {
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState<IFormRegisterData>({
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmitRegister = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    postRegister(values)
+      .then((res) => {
+        localStorage.setToken("accessToken", res.data.user.token);
+        // setIsLogin(!!token.getToken(ACCESS_TOKEN_KEY));
+        navigate("/", { replace: true });
+      })
+      .catch((err) => {
+        setError({
+          email: err.response.data.errors.email,
+          username: err.response.data.errors.username,
+          password: err.response.data.errors.password,
+        });
+      });
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -6,19 +54,24 @@ const SignUpPage = () => {
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-xs-center">Sign Up</h1>
             <p className="text-xs-center">
-              <a href="">Have an account?</a>
+              <Link to="/login">Have an account?</Link>
             </p>
 
             <ul className="error-messages">
-              <li>That email is already taken</li>
+              {error.email && <li>email {error.email}</li>}
+              {error.username && <li>username {error.username}</li>}
+              {error.password && <li>password {error.password}</li>}
             </ul>
 
-            <form>
+            <form onSubmit={onSubmitRegister}>
               <fieldset className="form-group">
                 <input
                   className="form-control form-control-lg"
                   type="text"
                   placeholder="Your Name"
+                  name="username"
+                  value={values.username}
+                  onChange={handleChange}
                 />
               </fieldset>
               <fieldset className="form-group">
@@ -26,6 +79,9 @@ const SignUpPage = () => {
                   className="form-control form-control-lg"
                   type="text"
                   placeholder="Email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
                 />
               </fieldset>
               <fieldset className="form-group">
@@ -33,6 +89,9 @@ const SignUpPage = () => {
                   className="form-control form-control-lg"
                   type="password"
                   placeholder="Password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
                 />
               </fieldset>
               <button className="btn btn-lg btn-primary pull-xs-right">
