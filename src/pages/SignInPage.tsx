8f9ec25aff11f9e3b3b36f182efc34d0ec3df63d
lsenaa <lsenaa3838@gmail.com>
2023-04-20 16:30:46 +0900

    fix: 로그인 폼 수정

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,6 +1,49 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { postLogin } from "../apis/users/users";
+
+interface IFormSigninData {
+  email: string;
+  password: string;
+}
 
 const SignInPage = () => {
+  const navigate = useNavigate();
+  const [values, setValues] = useState<IFormSigninData>({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({
+    email: "",
+    password: "",
+    emailOrPassword: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    postLogin(values)
+      .then((res) => {
+        localStorage.setToken("accessToken", res.data.user.token);
+        // setIsLogin(!!token.getToken("accessToken"));
+        navigate("/", { replace: true });
+      })
+      .catch((err) => {
+        setError({
+          email: err.response.data.errors.email,
+          password: err.response.data.errors.password,
+          emailOrPassword: err.response.data.errors["email or password"],
+        });
+      });
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -12,16 +55,22 @@ const SignInPage = () => {
             </p>
 
             <ul className="error-messages">
-              <li>email error</li>
-              <li>password error</li>
+              {error.email && <li>email {error.email}</li>}
+              {error.password && <li>password {error.password}</li>}
+              {error.emailOrPassword && (
+                <li>email or password {error.emailOrPassword}</li>
+              )}
             </ul>
 
-            <form>
+            <form onSubmit={onSubmitForm}>
               <fieldset className="form-group">
                 <input
                   className="form-control form-control-lg"
                   type="text"
                   placeholder="Email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
                 />
               </fieldset>
               <fieldset className="form-group">
@@ -29,6 +78,9 @@ const SignInPage = () => {
                   className="form-control form-control-lg"
                   type="password"
                   placeholder="Password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
                 />
               </fieldset>
               <button className="btn btn-lg btn-primary pull-xs-right">
