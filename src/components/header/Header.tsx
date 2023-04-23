4dd0c8e6240fc95475c5f77bda98843c5681ae8b
lsenaa <lsenaa3838@gmail.com>
2023-04-23 21:41:04 +0900

    fix: 헤더 스타일 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,8 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { UserContext } from "../../contexts/UserContext";
 import { useGetUser } from "../../hooks/useGetUser";
 
 const Header = () => {
-  const { isLogin } = useContext(UserContext);
   const { userData } = useGetUser();
-  console.log(userData);
 
   return (
     <nav className="navbar navbar-light">
@@ -16,20 +12,52 @@ const Header = () => {
         </Link>
         <ul className="nav navbar-nav pull-xs-right">
           <li className="nav-item">
-            <Link to="/">Home</Link>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/editor">New Article</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/settings">Settings</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/login">Sign in</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/register">Sign up</Link>
-          </li>
+
+          {userData ? (
+            <>
+              <li className="nav-item">
+                <Link to="/editor" className="nav-link">
+                  New Article
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/settings" className="nav-link">
+                  Settings
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to={`/profile/${userData.data.user.username}`}
+                  className="nav-link"
+                  // state={userData.username}
+                >
+                  <img
+                    className="user-pic"
+                    src={userData.data.user.image}
+                    alt="profile"
+                  />
+                  {userData.data.user.username}
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">
+                  Sign in
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/register" className="nav-link">
+                  Sign up
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
