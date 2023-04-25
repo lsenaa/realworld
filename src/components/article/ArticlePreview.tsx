66895778cf47f430d7284f12cc93a1bc399620f9
lsenaa <lsenaa3838@gmail.com>
2023-04-25 15:43:33 +0900

    refactor: props 수정

--- src/components/article/ArticlePreview.tsx
+++ src/components/article/ArticlePreview.tsx
@@ -1,45 +1,45 @@
 import { Link } from "react-router-dom";
 
 interface IArticle {
-  author: {
-    username: string;
-    image: string;
+  article: {
+    author: {
+      username: string;
+      image: string;
+    };
+    body?: string;
+    createdAt: string;
+    description: string;
+    favoritesCount: number;
+    slug: string;
+    tagList: string[];
+    title: string;
+    updatedAt?: string;
   };
-  body?: string;
-  createdAt: string;
-  description: string;
-  favoritesCount: number;
-  slug: string;
-  tagList: string[];
-  title: string;
-  updatedAt?: string;
 }
 
-const ArticlePreview = (props: { article: IArticle }) => {
-  console.log(props.article);
-
+const ArticlePreview = ({ article }: IArticle) => {
   return (
     <div className="article-preview">
       <div className="article-meta">
         {/* <Link to={`/profile/${username}`}> */}
-        <img src={props.article.author.image} alt="author" />
+        <img src={article.author.image} alt="author" />
         {/* </Link> */}
         <div className="info">
           <Link to="" className="author">
-            {props.article.author.username}
+            {article.author.username}
           </Link>
-          <span className="date">{props.article.createdAt}</span>
+          <span className="date">{article.createdAt}</span>
         </div>
         <button className="btn btn-outline-primary btn-sm pull-xs-right">
-          <i className="ion-heart"></i> {props.article.favoritesCount}
+          <i className="ion-heart"></i> {article.favoritesCount}
         </button>
       </div>
-      <Link to={`/article/${props.article.slug}`} className="preview-link">
-        <h1>{props.article.slug.split("-").join(" ").slice(0, -7)}</h1>
-        <p>{props.article.description}</p>
+      <Link to={`/article/${article.slug}`} className="preview-link">
+        <h1>{article.slug.split("-").join(" ").slice(0, -7)}</h1>
+        <p>{article.description}</p>
         <span>Read more...</span>
         <ul className="tag-list">
-          {props.article.tagList.map((tag: string, index: number) => (
+          {article.tagList.map((tag: string, index: number) => (
             <li
               className="tag-default tag-pill tag-outline ng-binding ng-scope"
               key={index}
