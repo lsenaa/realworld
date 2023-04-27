76e535efb5a9c83418ece4596ca3311c0830bfdd
lsenaa <lsenaa3838@gmail.com>
2023-04-27 17:39:19 +0900

    fix: 불필요한 코드 삭제

--- src/hooks/queries/useQueryUser.ts
+++ src/hooks/queries/useQueryUser.ts
@@ -1,16 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getUser } from "../../apis/users/users";
 
-export const useGetUser = () => {
-  const { data: userData } = useQuery({
-    queryKey: ["user"],
-    queryFn: getUser,
-    staleTime: 20000,
-  });
-
-  return { userData };
-};
-
 export const useUserQuery = () => {
   const { data: userData, isLoading: userIsLoading } = useQuery(["user"], () =>
     getUser()
