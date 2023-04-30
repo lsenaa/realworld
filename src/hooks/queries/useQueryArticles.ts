255fac67b6a287548abcd8b196b2d56227f55d37
lsenaa <lsenaa3838@gmail.com>
2023-04-30 18:30:21 +0900

    feat: article mutation 추가

--- src/hooks/queries/useQueryArticles.ts
+++ src/hooks/queries/useQueryArticles.ts
@@ -1,8 +1,11 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import {
+  DeleteArticle,
   getArticles,
   getArticlesSlug,
   getFeed,
+  postArticle,
+  putArticle,
 } from "../../apis/articles/articles";
 
 export const useArticlesQuery = () => {
@@ -16,11 +19,32 @@ export const useArticlesQuery = () => {
     getFeed()
   );
 
+  const { mutate: postArticleMutation } = useMutation(postArticle, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["articles"]);
+    },
+  });
+
+  const { mutate: putArticleMutation } = useMutation(putArticle, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["articles"]);
+    },
+  });
+
+  const { mutate: deleteArticleMutation } = useMutation(DeleteArticle, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["articles"]);
+    },
+  });
+
   return {
     data,
     feedData,
     listIsLoading,
     feedIsLoading,
+    postArticleMutation,
+    putArticleMutation,
+    deleteArticleMutation,
   };
 };
 
