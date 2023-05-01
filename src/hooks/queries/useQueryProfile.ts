6c42f6513bd9cc41b43bc22253e5e7cc0892ca14
lsenaa <lsenaa3838@gmail.com>
2023-05-01 19:52:58 +0900

    fix: follow mutation invalidateQueries 추가

--- src/hooks/queries/useQueryProfile.ts
+++ src/hooks/queries/useQueryProfile.ts
@@ -1,15 +1,25 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteFollow, getProfile, postFollow } from "../../apis/profiles";
 
 export const useProfileQuery = (username: string) => {
+  const queryClient = useQueryClient();
+
   const { data: profileData, isLoading: profileIsLoading } = useQuery(
     ["user"],
     () => getProfile(username)
   );
 
-  const { mutate: postFollowMutation } = useMutation(postFollow, {});
+  const { mutate: postFollowMutation } = useMutation(postFollow, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["articles"]);
+    },
+  });
 
-  const { mutate: deleteFollowMutation } = useMutation(deleteFollow, {});
+  const { mutate: deleteFollowMutation } = useMutation(deleteFollow, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["articles"]);
+    },
+  });
 
   return {
     profileData,
