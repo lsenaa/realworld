5f7c4dd74eafd4ab09b5363ab2ca437df06a2e1a
lsenaa <lsenaa3838@gmail.com>
2023-05-19 15:52:48 +0900

    fix: ArticlePage 로딩 추가

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -23,97 +23,109 @@ export interface IComment {
 const ArticlePage = () => {
   const params = useParams();
   const { isLogin } = useIsLogin();
-  const { articleData } = useArticleQuery(String(params.slug));
+  const { articleData, articleIsLoading } = useArticleQuery(
+    String(params.slug)
+  );
   const article: IArticle = articleData?.data.article;
   const { commentData } = useCommentQuery(String(params.slug));
   const comments: IComment[] = commentData?.data.comments;
 
   return (
     <div className="article-page">
-      <div className="banner">
-        <div className="container">
-          <h1>{article?.title}</h1>
+      {articleIsLoading ? (
+        <h1 style={{ textAlign: "center", fontWeight: "bold" }}>Loading...</h1>
+      ) : (
+        <>
+          <div className="banner">
+            <div className="container">
+              <h1>{article?.title}</h1>
 
-          <div className="article-meta">
-            <Link to={`/profile/${article?.author.username}`}>
-              <img src={article?.author.image} alt="profile" />
-            </Link>
-            <div className="info">
-              <Link
-                to={`/profile/${article?.author.username}`}
-                className="author"
-              >
-                {article?.author.username}
-              </Link>
-              <span className="date">
-                {convertDate(
-                  article?.updatedAt ? article?.updatedAt : article?.createdAt
-                )}
-              </span>
+              <div className="article-meta">
+                <Link to={`/profile/${article?.author.username}`}>
+                  <img src={article?.author.image} alt="profile" />
+                </Link>
+                <div className="info">
+                  <Link
+                    to={`/profile/${article?.author.username}`}
+                    className="author"
+                  >
+                    {article?.author.username}
+                  </Link>
+                  <span className="date">
+                    {convertDate(
+                      article?.updatedAt
+                        ? article?.updatedAt
+                        : article?.createdAt
+                    )}
+                  </span>
+                </div>
+                {isLogin ? <Buttons article={article} /> : <></>}
+              </div>
             </div>
-            {isLogin ? <Buttons article={article} /> : <></>}
           </div>
-        </div>
-      </div>
 
-      <div className="container page">
-        <div className="row article-content">
-          <div className="col-md-12">
-            <p>{article?.body}</p>
-            {article?.tagList.map((tag: string) => (
-              <li key={tag} className="tag-default tag-pill tag-outline">
-                {tag}
-              </li>
-            ))}
-          </div>
-        </div>
-
-        <hr />
-
-        <div className="article-actions">
-          <div className="article-meta">
-            <Link to={`/profile/${article?.author.username}`}>
-              <img src={article?.author.image} alt="profile" />
-            </Link>
-            <div className="info">
-              <Link
-                to={`/profile/${article?.author.username}`}
-                className="author"
-              >
-                {article?.author.username}
-              </Link>
-              <span className="date">
-                {convertDate(
-                  article?.updatedAt ? article?.updatedAt : article?.createdAt
-                )}
-              </span>
-            </div>
-            {isLogin ? <Buttons article={article} /> : <></>}
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12 col-md-8 offset-md-2">
-            {isLogin ? (
-              <>
-                <CommentWrite slug={String(params.slug)} />
-                {comments?.map((comment: IComment) => (
-                  <CommentList
-                    comment={comment}
-                    slug={String(params.slug)}
-                    key={comment.id}
-                  />
+          <div className="container page">
+            <div className="row article-content">
+              <div className="col-md-12">
+                <p>{article?.body}</p>
+                {article?.tagList.map((tag: string) => (
+                  <li key={tag} className="tag-default tag-pill tag-outline">
+                    {tag}
+                  </li>
                 ))}
-              </>
-            ) : (
-              <p style={{ textAlign: "center" }}>
-                <Link to="/login">Sign in</Link> or{" "}
-                <Link to="/register">sign up</Link> to add comments on this
-                article.
-              </p>
-            )}
+              </div>
+            </div>
+
+            <hr />
+
+            <div className="article-actions">
+              <div className="article-meta">
+                <Link to={`/profile/${article?.author.username}`}>
+                  <img src={article?.author.image} alt="profile" />
+                </Link>
+                <div className="info">
+                  <Link
+                    to={`/profile/${article?.author.username}`}
+                    className="author"
+                  >
+                    {article?.author.username}
+                  </Link>
+                  <span className="date">
+                    {convertDate(
+                      article?.updatedAt
+                        ? article?.updatedAt
+                        : article?.createdAt
+                    )}
+                  </span>
+                </div>
+                {isLogin ? <Buttons article={article} /> : <></>}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 col-md-8 offset-md-2">
+                {isLogin ? (
+                  <>
+                    <CommentWrite slug={String(params.slug)} />
+                    {comments?.map((comment: IComment) => (
+                      <CommentList
+                        comment={comment}
+                        slug={String(params.slug)}
+                        key={comment.id}
+                      />
+                    ))}
+                  </>
+                ) : (
+                  <p style={{ textAlign: "center" }}>
+                    <Link to="/login">Sign in</Link> or{" "}
+                    <Link to="/register">sign up</Link> to add comments on this
+                    article.
+                  </p>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
